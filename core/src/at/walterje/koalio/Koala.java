22f8ac28a94f2bfdefef942d1f7a6e94e83a2b99
walterje <walterje@edu.aau.at>
2017-04-27 17:15:44 +0200

    Own class for Koala

---
+++ core/src/at/walterje/koalio/Koala.java
@@ -0,0 +1,24 @@
+package at.walterje.koalio;
+
+import com.badlogic.gdx.math.Vector2;
+
+/** The player character, has state and state time, */
+public class Koala {
+
+        static float WIDTH;
+        static float HEIGHT;
+        static float MAX_VELOCITY = 10f;
+        static float JUMP_VELOCITY = 40f;
+        static float DAMPING = 0.87f;
+
+        enum State {
+            Standing, Walking, Jumping
+        }
+
+        final Vector2 position = new Vector2();
+        final Vector2 velocity = new Vector2();
+        State state = State.Walking;
+        float stateTime = 0;
+        boolean facesRight = true;
+        boolean grounded = false;
+}
